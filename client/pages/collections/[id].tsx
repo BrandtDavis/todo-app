07caf33d8cb5984ba7cb03d5d10f831ec3f4281f
Brandt Davis <brandtd77@gmail.com>
2022-12-21 08:36:43 -0700

    Fix json return issue by adding await

--- client/pages/collections/[id].tsx
+++ client/pages/collections/[id].tsx
@@ -1,9 +1,6 @@
 import Layout from '../../components/layout'
 import { getAllCollectionIds, getCollectionData } from '../../lib/collections'; 
 import { GetStaticProps, GetStaticPaths } from 'next'
-import { cp } from 'fs';
-import { getPositionOfLineAndCharacter } from 'typescript';
-
 
 export default function Collection({
     collectionData
@@ -26,19 +23,19 @@ export default function Collection({
     );
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const collectionData = getCollectionData(params?.id as string);
-    return {
-      props: {
-        collectionData,
-      },
-    };
-}
-
 export const getStaticPaths: GetStaticPaths = async () => {
     const paths = getAllCollectionIds()
     return {
      paths,
      fallback: false
     }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const collectionData = await getCollectionData(params?.id as string);
+    return {
+      props: {
+        collectionData,
+      },
+    };
 }
