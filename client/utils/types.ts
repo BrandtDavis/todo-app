47bfc1dc73fefce916da5d4a8c1c640a49792938
Brandt Davis <brandtd77@gmail.com>
2022-12-22 23:01:22 -0700

    Add connection file and types

---
+++ client/utils/types.ts
@@ -0,0 +1,16 @@
+//https://dev.to/alexmercedcoder/building-a-full-stack-todo-list-with-mongodb-nextjs-typescript-2f75
+
+// Interface to defining our object of response functions
+export interface ResponseFuncs {
+    GET?: Function
+    POST?: Function
+    PUT?: Function
+    DELETE?: Function
+  }
+  
+  // Interface to define our Todo model on the frontend
+  export interface Todo {
+    _id?: number
+    item: string
+    completed: boolean
+  }
