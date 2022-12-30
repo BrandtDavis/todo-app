afc486b4887e7a5b5a1a4b7354631df39e93349f
Brandt Davis <brandtd77@gmail.com>
2022-12-29 23:04:34 -0700

    Remove basic insert

--- client/utils/connection.ts
+++ client/utils/connection.ts
@@ -18,7 +18,8 @@ export const connect = async () => {
   // OUR TODO MODEL
   const Todo = mongoose.models.Todo || mongoose.model("Todo", TodoSchema)
 
-  const todo = new Todo({name: 'Bill '})
-  todo.save().then(() => console.log('todo saved')) 
+//   const todo = new Todo({name: 'Bill '})
+//   todo.save().then(() => console.log('todo saved')) 
+
   return { conn, Todo }
 }
