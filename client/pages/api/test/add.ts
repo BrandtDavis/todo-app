e96aaef6c37118b89f2a18d7b6de7e5a673fb999
Brandt Davis <brandtd77@gmail.com>
2023-01-02 15:36:51 -0700

    Add hard coded insert operation

--- client/pages/api/test/add.ts
+++ client/pages/api/test/add.ts
@@ -6,11 +6,12 @@ import { connect } from '../../../utils/connection'
 import Test from '../../../models/testModels'
 
 async function addTest(req: NextApiRequest, res:NextApiResponse) {
+
     const { name, numItems } = req.body;
     await connect();
 
-    const test = await Test.create(req.body)
+     const test = await Test.create({name: 'bob', numItems: 67})
 
-    // res.json({test})
+     res.json({test})
 }
 export default addTest;
