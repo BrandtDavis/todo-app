5390d8823553f3685b428764ce014c6cc0a5eeeb
Brandt Davis <brandtd77@gmail.com>
2023-01-02 16:29:25 -0700

    Add button to call basic API

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,6 +6,7 @@ import { getSortedCollectionsData } from '../lib/collections';
 import { GetStaticProps, GetStaticPaths, GetServerSideProps} from 'next';
 import Date from '../components/date'
 
+
 export default function Home({ allCollectionsData }:  
   { allCollectionsData: {
     date: string 
@@ -14,6 +15,23 @@ export default function Home({ allCollectionsData }:
   }[]
 }) {
   console.log(allCollectionsData)
+
+
+  const createTest = async () => {
+    const res = await fetch('/api/test/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'Name',
+        numItems: 160,
+      })
+    })
+    const data = await res.json();
+    console.log(data)
+  }
+
   return (
     <Layout home>
       <Head>
@@ -22,13 +40,15 @@ export default function Home({ allCollectionsData }:
       <section className={utilStyles.headingMd}>
         <h1>The todo list web app</h1>
         <p>You may have multiple lists, each with their own name</p>
+
+        <button onClick={createTest}>Add to Mongo</button>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allCollectionsData.map(({ id, date, title }) => (
-            <li className={utilStyles.lisdtItem} key={id}>
+            <li className={utilStyles.listItem} key={id}>
              <Link href={`/collections/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
