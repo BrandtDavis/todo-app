5e67ea06b4aaa7945714d2d91eb7ff9d7f0e5e65
Brandt Davis <brandtd77@gmail.com>
2023-01-03 23:12:27 -0700

    Rename file, refactor names in file

--- client/models/ToDoList.ts
+++ client/models/ToDoList.ts
@@ -1,12 +1,12 @@
 import { Schema, model, models } from 'mongoose';
   
   // OUR TODO SCHEMA
-  const TodoSchema = new Schema({
+  const ToDoListSchema = new Schema({
     name: String,
     items: Array<String>,
   })
 
   // OUR TODO MODEL
-  const Todo = models.Todo || model("Todo", TodoSchema)
+  const TodoList = models.Todo || model("Todo", ToDoListSchema)
 
-  export default Todo;
+  export default TodoList;
