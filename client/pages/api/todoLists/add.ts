a66d599aa3e4259008433a4c3f424d3d12dc96c3
Brandt Davis <brandtd77@gmail.com>
2023-01-04 23:08:16 -0700

    Add status code to response situations

--- client/pages/api/todoLists/add.ts
+++ client/pages/api/todoLists/add.ts
@@ -7,8 +7,16 @@ import Todo from '../../../models/ToDoList'
 
 async function addTest(req: NextApiRequest, res:NextApiResponse) {
 
+    const body = req.body
+    console.log('body: ', body)
+
+    if(!body.name) {
+        return res.status(400).json({ data: 'Name not found, my dude' })
+    }
+
     await connect();
-    const toDo = await Todo.create(req.body)
-    res.json({toDo})
+    const toDo = await Todo.create(body)
+    
+    res.status(200).json({toDo})
 }
 export default addTest;
