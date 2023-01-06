04a6f25789a6adcde837f513d9f7d227b624f4f5
Brandt Davis <brandtd77@gmail.com>
2023-01-05 23:10:18 -0700

    Update error handling

--- client/pages/api/todoLists/add.ts
+++ client/pages/api/todoLists/add.ts
@@ -10,13 +10,13 @@ async function addTest(req: NextApiRequest, res:NextApiResponse) {
     const body = req.body
     console.log('body: ', body)
 
-    if(!body.name) {
-        return res.status(400).json({ data: 'Name not found, my dude' })
+    if(!body.title) {
+        return res.status(400).json({ data: 'Title not found, my dude' })
     }
 
     await connect();
     const toDo = await Todo.create(body)
-    
+    console.log("todo: ", toDo)
     res.status(200).json({toDo})
 }
 export default addTest;
