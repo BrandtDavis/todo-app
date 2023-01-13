280dadd0096449a4cc18abb53ab29dc42e635390
Brandt Davis <brandtd77@gmail.com>
2023-01-12 22:08:39 -0700

    Update input fields to store values in state

--- client/pages/forms/addToDoDetails.tsx
+++ client/pages/forms/addToDoDetails.tsx
@@ -2,11 +2,6 @@ import ToDoFormLayout from '../../components/layouts/toDoFormLayout';
 import { useState } from 'react';
 
 
-
-const myFunc = () => {
-    
-}
-
 export default function AddTodoForm() {
 
 
@@ -18,13 +13,9 @@ const [priority, setPriority] = useState("");
 const [color, setColor] = useState("");
 const [numItems, setNumItems] = useState("");
 
-// const [formValues, setFormValues] = useState(
-//     { 
-//         title: "", 
-//         priority: "",
-//         color: "", 
-//         numItems: "" 
-//     });
+const createToDoList = () => {
+    
+}
 
     return (
         <ToDoFormLayout>
@@ -33,23 +24,42 @@ const [numItems, setNumItems] = useState("");
             {/* <form action="/api/todoLists/add" method="post" className="toDoForm" > */}
             <form className="toDoForm" >
                 <label htmlFor="title"> Title </label>
-                <input type="text" id="title" name="title" />
+                <input 
+                    type="text" 
+                    id="title" 
+                    name="title" 
+                    onChange={e => setTitle(e.target.value)}
+                />
 
                 <label htmlFor="priority"> Priority </label>
-                <select id="priority" name="priority">
+                <select 
+                    id="priority" 
+                    name="priority"
+                    onChange={e => setPriority(e.target.value)}
+                >
                     <option value="low">Low</option>
                     <option value="medium">Medium</option>
                     <option value="high">High</option>
                 </select>
 
                 <label htmlFor="color"> Color </label>
-                <input type="text" id="color" name="color" />
+                <input 
+                    type="text" 
+                    id="color" 
+                    name="color" 
+                    onChange={e => setColor(e.target.value)}
+                />
 
                 <label htmlFor="numItems"> Number of Items </label>
-                <input type="number" id="numItems" name="numItems" />
+                <input 
+                    type="number" 
+                    id="numItems" 
+                    name="numItems" 
+                    onChange={e => setNumItems(e.target.value)}
+                />
 
 
-                <button type="button" onClick={(e) => myFunc()}> Next </button>
+                <button type="button" onClick={(e) => createToDoList()}> Next </button>
 
             </form>
 
