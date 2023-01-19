078f797a5acfcaebe9cb1cacb3f3ed05aecd8c6b
Brandt Davis <brandtd77@gmail.com>
2023-01-18 23:51:39 -0700

    Implement function to create todolist

--- client/lib/toDoLists.ts
+++ client/lib/toDoLists.ts
@@ -1,6 +1,17 @@
-const createToDoList = () => {
-
-
+const createToDoList = async (
+        title: string,
+        priority: string,
+        color: string,
+) => {
+    const response = await fetch('/api/todoLists/add', {
+        method: 'POST',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, priority, color })
+    });
+    return response.json()
 }
 
 export default createToDoList;
