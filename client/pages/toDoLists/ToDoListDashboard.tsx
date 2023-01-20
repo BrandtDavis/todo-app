b4521843dbc059c66a2838f54acc54c03ff25ca6
Brandt Davis <brandtd77@gmail.com>
2023-01-19 23:49:27 -0700

    Call API on FE

--- client/pages/toDoLists/ToDoListDashboard.tsx
+++ client/pages/toDoLists/ToDoListDashboard.tsx
@@ -1,8 +1,25 @@
 export default function ToDoListDashboard() {
+   
+   const getToDoList = async (
+
+   ) => {
+    const response = await fetch('/api/todoLists/getAll', {
+        method: 'GET',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+    // console.log(response)
+    return response.json()
+   }
+   
     return (
         <div>
 
             <h1>Your ToDo Lists</h1>
+
+            <button onClick={getToDoList}>Click Meh</button>
    
         </div>
     );
