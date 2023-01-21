855c1479bd78482f67a3b6978ef44db538270ce9
Brandt Davis <brandtd77@gmail.com>
2023-01-21 07:18:17 -0700

    Dynamically generate components based on fetch results

--- client/pages/toDoLists/ToDoListDashboard.tsx
+++ client/pages/toDoLists/ToDoListDashboard.tsx
@@ -18,21 +18,21 @@ export default function ToDoListDashboard() {
         }]
     );
 
-    // useEffect( () => {
-    //     fetch('/api/todoLists/getAll').then(response => {
-    //         // if(response) {
-    //             return response.json()
-    //         // }
-    //     }).then(jsonRes => setToDos(Object.values(jsonRes)))
-    // }, []);
+    useEffect( () => {
+        fetch('/api/todoLists/getAll').then(response => {
+            // if(response) {
+                return response.json()
 
-    // const toDoArrays = Object.values(toDos)
-    // console.log(toDoArrays)
+            // }
+        }).then(arrayRes => setToDos(arrayRes))
+    }, []);
+
+    const toDoArrays = Object.values(toDos)[0]
+    console.log(toDoArrays[0])
     return (
         <div>
             <h1>Your ToDo Lists</h1>           
-            {/* {
-                toDos.map( (toDo, i) =>  */}
+
                     <div className="toDoItem" >
                         <h3 className="toDoItemH3" >{toDo[0].title}</h3>
                         <div>
@@ -43,8 +43,22 @@ export default function ToDoListDashboard() {
                             </ul>
                         </div>
                     </div>    
-                {/* )
-            } */}
+
+
+            {
+                toDoArrays.map( (toDo, i) => 
+                    <div className="toDoItem" key={i}>
+                        <h3 className="toDoItemH3" key={i}>{toDo.title}</h3>
+                        <div>
+                            <ul>                   
+                                <li>This is an item</li>
+                                <li>This is an item</li>
+                                <li>This is an item</li>
+                            </ul>
+                        </div>
+                    </div>    
+                )
+            } 
    
         </div>
     );
