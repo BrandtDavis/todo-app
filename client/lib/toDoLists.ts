e3f2b5416bb5d04e6f8af72eebeeb4f19b504f1b
Brandt Davis <brandtd77@gmail.com>
2023-01-30 23:44:41 -0700

    Add interface

--- client/lib/toDoLists.ts
+++ client/lib/toDoLists.ts
@@ -1,15 +1,26 @@
+import ToDoItemSchema from "../models/ToDoItem";
+
+interface ToDoItem {
+    toDoItemName: string,
+    completed: boolean,
+}
+
 const createToDoList = async (
         title: string,
         priority: string,
         color: string,
+        toDoItems: string[],
 ) => {
-    const response = await fetch('/api/todoLists/add', {
+    console.log(JSON.stringify({ title, priority, color, toDoItems }))
+
+
+    const response = await fetch('/api/todoLists/addToDoList', {
         method: 'POST',
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ title, priority, color })
+        body: JSON.stringify({ title, priority, color, toDoItems })
     });
     return response.json()
 }
