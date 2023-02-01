ef381c8a10cda8bf70fccb909c71f3e6173200d1
Brandt Davis <brandtd77@gmail.com>
2023-01-31 23:47:02 -0700

    Implement function to get all todo lists

--- client/lib/getAllToDoLists.ts
+++ client/lib/getAllToDoLists.ts
@@ -1,5 +1,10 @@
+import { connect } from '../utils/connection';
+import Todo from '../models/ToDoList'
+
 const getAllToDoLists = async () => {
 
+    await connect();
+    const toDos = await Todo.find();
 }
 
 export default getAllToDoLists;
