1dd613546a947462a62aadfba98c5213449964d9
Brandt Davis <brandtd77@gmail.com>
2023-02-08 08:49:28 -0700

    Add button to call new api for testing

--- client/pages/forms/addToDoItems.tsx
+++ client/pages/forms/addToDoItems.tsx
@@ -1,15 +1,21 @@
 import ToDoFormLayout from '../../components/layouts/toDoFormLayout';
 
 export default function AddToDoListItems(){
+
+    const getItem = async() => {
+        const res = await fetch('/api/todoLists/getToDoItemById');
+        console.log(res)
+    }
+
     return (
         <ToDoFormLayout>
         <form action="">
 
-            <input type="text" placeholder="Suhhhhh" />
+            <input type="text" placeholder="Customize your items" />
 
             <a href="/forms/addToDoDetails">Previous</a>
 
-            <button type="submit">Submit</button>
+            <button type="button" onClick={getItem} >TEST</button>
 
         </form>
         </ToDoFormLayout>
