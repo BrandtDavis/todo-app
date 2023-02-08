240918372126580f6bd4b01301d57220fd29f6ad
Brandt Davis <brandtd77@gmail.com>
2023-02-08 08:49:03 -0700

    Add basic api to get an item by its id

--- client/pages/api/todoLists/getToDoItemById.ts
+++ client/pages/api/todoLists/getToDoItemById.ts
@@ -4,12 +4,15 @@ import {
 } from 'next';
 import { connect } from '../../../utils/connection'
 import ToDoItem from '../../../models/ToDoItem'
+import mongoose from 'mongoose';
 
 async function getToDoItemById(req: NextApiRequest, res:NextApiResponse) {
     const body = req.body
 
     await connect();
-    const toDoItem= await ToDoItem.find(body)
-    res.status(200).json(toDoItem)
+    let id = new mongoose.Types.ObjectId(body._id);
+    const toDoItem= await ToDoItem.find({_id: id }).exec()
+    console.log(toDoItem)
+    return res.status(200).json(toDoItem)
 }
 export default getToDoItemById;
